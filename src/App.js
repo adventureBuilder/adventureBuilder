d4152161c8bd9e9230c64936b929a329352c7e79
BlakeMAdams <blakemadams@gmail.com>
2017-10-16 12:19:52 -0600

    npm installed dependencies

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+
+          conflicting with Paul
         </p>
       </div>
     );

5dfd5c71346c8f6f81769b5e6f2dc8922f60d991
Daniel Valentine <mrdanvalentine@gmail.com>
2017-10-17 14:25:02 -0600

    set up dummy data files

--- db/dummyData.sql
+++ db/dummyData.sql
@@ -1,6 +1,6 @@
-----------------------
---- create a class ---
-----------------------
+-- --------------------
+-- - create a class ---
+-- --------------------
 
 INSERT INTO classes 
 (
@@ -22,9 +22,9 @@ values (
     18
 );
 
--------------------
---- create user ---
--------------------
+-- -----------------
+-- - create user ---
+-- -----------------
 
 INSERT INTO users
 (
@@ -40,6 +40,164 @@ values (
     'http://placekitten.com/234/250'
 );
 
-----------------------
----create character---
-----------------------
+-- --------------------
+-- -create character---
+-- --------------------
+INSERT INTO character
+(
+    character_name, 
+    gender, 
+    dexterity,
+    strength,
+    charisma,
+    health_points,
+    alive,
+    class_ID,
+    user_id
+) 
+values (
+    'HarrisonFord', 
+    'female', 
+    1,
+    2,
+    6,
+    19,
+    1,
+    1,
+    1
+);
+
+-- ----------------
+-- -create story---
+-- ----------------
+insert into stories
+(
+  story_name,
+  user_ID ,
+  story_description,
+  story_level,
+  completed_at
+ )
+ values
+ (
+    'harrison for punches ryan gosling',
+    1,
+    'it actually happened',
+    1,
+    now()
+);
+-- -------------------------------------
+-- -create encounter bacground image ---
+-- -------------------------------------
+
+insert into encounter_background_images (
+  image_name,
+  image_src
+)
+values
+(
+    'Giant Cat attack',
+    'http://placekitten.com/300/250'
+);
+
+-- ----------------------
+-- - create encounter ---
+-- ----------------------
+
+insert into encounters (
+  encounter_name,
+  encounter_description,
+  story_ID,
+  encounter_background_images_ID 
+)
+values (
+    'Giant Cat attack',
+    'you are attacked by a giant cat with fangs',
+    1,
+    1
+);
+
+-- -------------------------
+-- - create option image ---
+-- -------------------------
+
+insert into option_images (
+  image_name,
+  image_src 
+)
+values (
+    'sneak cat',
+    'http://placekitten.com/400/441'
+);
+
+-- ---------------------
+-- create 3 options ---
+-- ---------------------
+insert into encounter_options (
+  encounter_ID,
+  option_name,
+  option_description,
+  option_odds,
+  options_pass_case,
+  success_text,
+  failed_text,
+  success_encounter,
+  failed_encounter,
+  option_images_ID,
+  health_consquences
+)
+values
+(
+    1,
+    'sneak',
+    'try to sneak past the evil cat',
+    '1d4+dex',
+    3,
+    'you snooked away',
+    'the cat bite you',
+    1,
+    1,
+    1,
+    3
+),
+(
+    1,
+    'flirt',
+    'try to make the cat fall in love with your devilish charm',
+    '1d4+cha',
+    3,
+    'you seduced the cat and now you have a have cat have person baby',
+    'the cat bite you',
+    1,
+    1,
+    1,
+    3
+),
+(
+    1,
+    'fight',
+    'kinda rude tho?',
+    '1d4+str',
+    3,
+    'you killed away the cat',
+    'the cat bite you',
+    1,
+    1,
+    1,
+    3
+);
+
+---------------------------------------------------------------
+-- -create relation between character and story and encounter--
+---------------------------------------------------------------
+
+insert into story_has_character (
+  story_ID,
+  character_ID,
+  last_encounter_ID
+)
+values (
+    1,
+    1,
+    1
+);

5a666509e1bf97ba4a9bda70a0cd3a11ce4e22f9
CoquiFrog <polzer81@hotmail.com>
2017-10-18 10:24:15 -0600

    little bit of work on story view components

--- src/components/StorySelection/StorySelection.js
+++ src/components/StorySelection/StorySelection.js
@@ -5,8 +5,22 @@ import React, { Component } from 'react';
 export default class StorySelection extends Component {
     render () {
         return (
-            <div>
+            <div id="section1">
+            
+            <div>    
             StorySelection
+            </div>
+            <h1 className="viewTitle">Stories</h1>
+            {storiesArray.map((story, i) => {
+                return <ViewStory key={i} story={story}/>})}
+
+            {/* this.props.bag.map((garment, i) => {
+    return <Garment key={i} index={i} garment={garment} />; */}
+            <div>
+            <Link to='/tavern'><button>Back to Tavern</button></Link>
+
+            <li><a href="#section1">Back to top</a></li>
+            </div>
             </div>
         );
     }
--- src/components/ViewStory/ViewStory.js
+++ src/components/ViewStory/ViewStory.js
@@ -3,9 +3,30 @@
 import React, { Component } from 'react';
 
 export default class ViewStory extends Component {
-    render () {
+    render() {
         return (
+            <div className="storyContainer">
+                <div className="storyHeader">
+                <div className="storyTitle">
+                    Story Name
+                </div>
+                        
+                <div className="storyLevel">
+                Level: X
+                </div>
+                </div>
+            <div className="storyDescription">story description: There once was a boy named Jerry who lived in the woods.  He had no mother and was chased my a horse.  The end.</div>
             
+            <div className="storyFooter">
+            
+            <div className="storyAuthor"><p>written by:</p><h2>User Name</h2></div>
+            <button className="beginStory">Begin story!</button>
+
+            </div>
+            
+            
+
+            </div>
         );
     }
 }

e12e2e86d0df5ef339d4106c16e40d9f9f6c0362
BlakeMAdams <blakemadams@gmail.com>
2017-10-19 12:24:17 -0600

    committing to merge group pulls

--- src/components/MyCharacters/NewCharacter/NewCharacter.js
+++ src/components/MyCharacters/NewCharacter/NewCharacter.js
@@ -4,9 +4,51 @@
 import React, { Component } from 'react';
 
 export default class NewCharacter extends Component {
-    render () {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+
+    }
+    // BARB CHA-1 DEX0 STR2
+    // BARD CHA2 DEX-1 STR0
+    // ROGUE CHA0 DEX2 STR-1
+
+    changeChar(option, value) {
+        this.setState({
+            option: value
+        })
+    }
+    render() {
         return (
-            
+            <div className="new-character">
+
+                <h1>Create A Character</h1>
+
+                <div className="new-character-block">
+                    <div className="char-row">
+                        <h3>Name: </h3>
+                        <input type="text" />
+                    </div>
+                    <div className="char-row">
+                        <h3>Class: </h3>
+                        <select className="select-box" name="class" onChange={(e) => this.changeChar(e.target.name, e.target.value)}>
+                            <option value="Barbarian">Barbarian</option>
+                            <option value="Bard">Bard</option>
+                            <option value="Rogue">Rogue</option>
+                        </select>
+                    </div>
+
+                    <div className="char-row">
+                        <h3>Srength(STR)</h3>
+                        
+                    </div>
+
+
+
+                </div>
+            </div>
         );
     }
 }

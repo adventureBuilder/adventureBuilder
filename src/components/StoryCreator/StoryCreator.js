0722daa3e58a6569767902154a94c4e96a65934b
Daniel Valentine <mrdanvalentine@gmail.com>
2017-10-20 11:11:27 -0600

    stopping to merge

---
+++ src/components/StoryCreator/StoryCreator.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class StoryCreator extends Component {
+    render () {
+        return (
+            <div>
+                Story Creator
+            </div>
+            
+        );
+    }
+}
--- src/router.js
+++ src/router.js
@@ -12,6 +12,10 @@ import Tavern from './components/Tavern/Tavern';
 import StorySelection from './components/StorySelection/StorySelection';
 // the routing for our story selection menu
 import NewCharacter from './components/MyCharacters/NewCharacter/NewCharacter';
+// the routing for our new Story
+import NewStory from './components/NewStory/NewStory';
+// the routing for our story creator
+import StoryCreator from './components/StoryCreator/StoryCreator';
 
 
 export default (
@@ -22,5 +26,7 @@ export default (
         <Route path='/tavern' component={Tavern}/>
         <Route path='/storyselection' component={StorySelection}/>
         <Route path='/newcharacter' component={NewCharacter}/>
+        <Route path='/newstory' component={NewStory}/>
+        <Route path='/storycreator/:id' component={StoryCreator}/>
     </Switch>
 )

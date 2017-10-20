6bbc06b6bd7deb4d076e9fc659ee3c14d6a4b1e3
BlakeMAdams <blakemadams@gmail.com>
2017-10-20 10:47:44 -0600

    tweak on newcharacter removing withBrowser module

--- src/components/MyCharacters/NewCharacter/NewCharacter.js
+++ src/components/MyCharacters/NewCharacter/NewCharacter.js
@@ -3,11 +3,10 @@
 
 import React, { Component } from 'react';
 import axios from 'axios';
-import {withRouter} from "react-router-dom";
 
 import Menu from './../../Menu/Menu';
 
-class NewCharacter extends Component {
+export default class NewCharacter extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -27,9 +26,7 @@ class NewCharacter extends Component {
             this.setState({
                 classes: res.data
             })
-
         })
-
     }
 
     changeChar(option, value) {
@@ -114,4 +111,3 @@ class NewCharacter extends Component {
         );
     }
 }
-export default withRouter(NewCharacter);

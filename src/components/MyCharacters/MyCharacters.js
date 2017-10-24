c2fe9b891ac6412a013101f9d8f9e97a80b141fb
BlakeMAdams <blakemadams@gmail.com>
2017-10-24 08:34:29 -0600

    merging master

--- src/components/MyCharacters/MyCharacters.js
+++ src/components/MyCharacters/MyCharacters.js
@@ -4,10 +4,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { CSSTransitionGroup } from 'react-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import { getCharacters, getSelectedCharacter } from '../../ducks/reducer';
 import CharacterDisplay from '../CharacterDisplay/CharacterDisplay';
+
+// var CSSTransitionGroup = require('react-transition-group/CSSTransitionGroup')
+const styles = {
+    transition: 'all 1s ease-out'
+};
 
 class MyCharacters extends Component {
     constructor(props) {
@@ -16,7 +21,9 @@ class MyCharacters extends Component {
             characterList: [],
             selectedCharacterId: null,
             characterIdModuleToShow: null,
-            show: false
+            show: false,
+            opacity: 0
+
         }
         this.charDisplayDrop = this.charDisplayDrop.bind(this);
     }
@@ -33,20 +40,34 @@ class MyCharacters extends Component {
 
 
     //this.setState({selectedCharacterId: character})
-
-    charDisplayDrop(e){
-        
-        this.setState({ characterIdModuleToShow: e.target.id
+    onHide() {
+        this.setState({
+            opacity: 1
         })
+    }
+
+    charDisplayDrop(e) {
+        if (this.state[e.target.id]) {
+            this.setState({
+                [e.target.id]: false
+            })
+        }
+        else {
+            this.setState({
+                characterIdModuleToShow: e.target.id,
+                [e.target.id]: true
+            })
+        }
     }
 
     render() {
 
-        console.log(this.props.selectedCharacter, `this is the selected character`)
+        // console.log(this.props.selectedCharacter, `this is the selected character`)
         //console.log(this.state.showModule, this.state.characterIdModuleToShow, `state of showModule`)
 
-        const characterList = this.props.characters.map((character, i) =>
-            <div className="tavern-char-container" key={i}>
+        const characterList = this.props.characters.map((character, i) => {
+            <div>
+
                 <div className="tavern-char-row">
                     <div className="arrow-dropdown"></div>
                     <div className="tavern-char-name-container">
@@ -55,17 +76,27 @@ class MyCharacters extends Component {
                     <Link to={`/storyselection`}><button className="select-btn" id={character.character_id} onClick={() => { this.props.getSelectedCharacter(character.character_id); }}>PLAY</button></Link>
                 </div>
 
-                <div>{(this.state.characterIdModuleToShow == character.character_id+'NameButton') ? <CharacterDisplay character={character} /> : ''}</div>
-            </div>)
+
+                <div className={`tavern-char-container ${this.state[character.character_id] && 'show'}`} key={character.character_id}>
+                    {/* <CSSTransition key={i} classNames="example" timeout={{ enter: 500, exit: 300 }}> */}
+                    {(this.state.characterIdModuleToShow == character.character_id + 'NameButton') && <CharacterDisplay character={character} /> }
+                    {/* </CSSTransition> */}
+
+                </div>
+            </div>
+
+        }
+        );
 
         return (
             <div className="my-characters-list">
                 <h3 className="sub-title">My Characters:</h3>
                 <div className="block-card">
-                {/* <CSSTransitionGroup  transitionName="example"
+                    {/* <CSSTransitionGroup  transitionName="example"
                 transitionEnterTimeout={500}
-        transitionLeaveTimeout={300}>*/}
-                {characterList}{/*</CSSTransitionGroup> */}
+        transitionLeaveTimeout={300}>   <TransitionGroup>*/}
+                   {characterList}
+                    {/* </TransitionGroup></CSSTransitionGroup> */}
 
                     <Link to={`/newcharacter`}><button className="btn">Start New Character</button></Link>
 

3e2087702ead01516c57db81cb73a0b8096c6c94
CoquiFrog <polzer81@hotmail.com>
2017-10-25 09:25:06 -0600

    tried to fix animations but didn't succeed yet

--- src/components/MyCharacters/MyCharacters.js
+++ src/components/MyCharacters/MyCharacters.js
@@ -30,13 +30,13 @@ class MyCharacters extends Component {
         this.setState({ characterList: nextProps.characterList })
     };
 
-    openSlide(){
+    openSlide() {
         this.setState({
             slideVisible: !this.state.slideVisible
         })
     }
 
-   
+
 
     charDisplayDrop(e) {
         if (this.state[e.target.id]) {
@@ -56,37 +56,35 @@ class MyCharacters extends Component {
 
         // console.log(this.props.selectedCharacter, `this is the selected character`)
         //console.log(this.state.showModule, this.state.characterIdModuleToShow, `state of showModule`)
-       
-        const characterList = this.props.characters.filter(char=> char.alive).map((character, i) =>
+
+        const characterList = this.props.characters.filter(char => char.alive).map((character, i) =>
             <div className="tavern-char-container" key={i}>
                 <div className="tavern-char-row">
                     <div className="arrow-dropdown"></div>
                     <div className="tavern-char-name-container">
-                       
+
                         <button className="tavern-char-name" id={`${character.character_id}NameButton`} value={character.character_name} onClick={() => this.setState({ characterIdModuleToShow: character.character_id, slideVisible: !this.state.slideVisible })}><span className="down-arrow">&#9660;</span>  {character.character_name}</button>
                     </div>
                     <Link to={`/storyselection`}><button className="select-btn" id={character.character_id} onClick={() => { this.props.getSelectedCharacter(character.character_id); }}>PLAY</button></Link>
                 </div>
 
 
-                <div className={`${this.state[character.character_id] && 'show'}`} key={character.character_id}>
-                    
-                    {(this.state.characterIdModuleToShow == character.character_id + 'NameButton') && <CharacterDisplay character={character} slideVisible={this.state.slideVisible} openSlide={this.openSlide}/>}
-              
+                <div>{(this.state.characterIdModuleToShow === character.character_id) ? <CharacterDisplay character={character} slideVisible={this.state.slideVisible} openSlide={this.openSlide}/> : ''}
+
 
                 </div>
             </div>
 
-        
+
         );
 
         return (
             <div className="my-characters-list">
                 <h3 className="sub-title">My Characters:</h3>
                 <div className="block-card">
-                   
+
                     {characterList}
-                    
+
                     <Link to={`/newcharacter`}><button className="btn">Start New Character</button></Link>
 
                 </div>

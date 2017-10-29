731069c22ecca4fe491c2df56d46017faeeeb3ae
BlakeMAdams <blakemadams@gmail.com>
2017-10-29 08:55:04 -0600

    added create char button and styled

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,26 +20,42 @@ export default function About() {
 				<h1 className="view-title">About Adventure Builder</h1>
 				<div className="block-card-inner">
 					<div className="mobile-pad">
-					<p>Adventure Builder is a DevMountain student project designed to empower the player with the freedom to create their own story adventure and then journey through it.
+						<p>Adventure Builder is a DevMountain student project designed to empower the player with the freedom to create their own story adventure and then journey through it.
 					</p>
-					<h3>Battle Roll</h3>
-					<p>No journey encounter will be quite the same with our 'Battle Roll' which will roll a 1d6 die and check the result against the condition for success of each encounter.
+						<h3>Battle Roll</h3>
+						<p>No journey encounter will be quite the same with our 'Battle Roll' which will roll a 1d6 die and check the result against the condition for success of each encounter.
 					</p>
-					<h3>Character Stats</h3>
-					<p>The class you choose and its strengths will influence how your character attempts to overcome each encounter. The Battle Roll will be enhanced (or reduced) by the corresponding stat to determine the outcome.  Your character must then face the consequences of the 'Success' or 'Failure' before continuing on their journey.
+						<h3>Character Stats</h3>
+						<p>The class you choose and its strengths will influence how your character attempts to overcome each encounter. The Battle Roll will be enhanced (or reduced) by the corresponding stat to determine the outcome.  Your character must then face the consequences of the 'Success' or 'Failure' before continuing on their journey.
 					</p>
-					<h3>Build & Share Adventures</h3>
-					<p>You can create an adventure using the story builder tool, and share it for other players to journey through.  Use your imagination to forge new stories with encounters, obstacles, and path choices and decide the fate of those that succeed and fail your adventure.</p>
-					<h4><i>Travel across beautiful landscapes, encounter dangerous opposition, overcome odds of the dice, and become a legendary hero.</i></h4>
-					
+						<h3>Build & Share Adventures</h3>
+						<p>You can create an adventure using the story builder tool, and share it for other players to journey through.  Use your imagination to forge new stories with encounters, obstacles, and path choices and decide the fate of those that succeed and fail your adventure.</p>
+						<h4><i>Travel across beautiful landscapes, encounter dangerous opposition, overcome odds of the dice, and become a legendary hero.</i></h4>
+
 					</div>
 				</div>
 
-				<div className="block-card">
+				<div className="tech-used-container">
+					<div className="block-card">
+
+						<h1 className="sub-title">Tech Used</h1>
+						<div className="block-card-inner">
+							<div className="tech-used">
+								<div>ReactJS</div>
+								<div>Redux</div>
+								<div>NodeJS</div>
+								<div>PostgreSQL</div>
+								<div>Sass</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="meet-team block-card">
 
 					<h1 className="sub-title">Meet The Team</h1>
 					<div className="block-card-inner">
-						<p></p>
+
 
 
 						<div className="team-row">

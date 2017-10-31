0c79f7fbd535c6355a0b05455899d2c0d0a78480
BlakeMAdams <blakemadams@gmail.com>
2017-10-30 18:03:23 -0600

    adding files from victorias repo that had conflicts

--- src/components/StoryCreator/StoryCreator.js
+++ src/components/StoryCreator/StoryCreator.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import EncounterDisplay from './EncounterDisplay/EncounterDisplay';
 import AddEncounter from './AddEncounter/AddEncounter';
 import AddOption from './AddOption/AddOption';
+import Menu from '../Menu/Menu';
 // import './StoryCreator.css'
 
 export default class StoryCreator extends Component {
@@ -137,24 +138,33 @@ export default class StoryCreator extends Component {
                 break;
 
             default:
-                view = 'Click add encounter or option to start editing';
+                view = '';
                 break;
         }
         return (
             <div>
-                <h1>Story Creator</h1>
-                {this.state.story && this.state.story.story_name}
-                <div className='creator_container'>
-                    <div className='encounters_display_container'>
-                        {encounters}
-                        {encounters.length > 0 && <button onClick={this.openNewEncounter}>Add Encounter</button>}
-                        {this.isValid() && <button onClick={_=>this.completeStory()}>Complete Story</button>}
-                    </div>
 
+                <Menu/>
+
+                <h1>Story Creator</h1>
+
+                {this.state.story && this.state.story.story_name}
+                    <div className='page'>
+                        <h2>Story Creator</h2>
+                    <div className='creator_container'>
+                        {encounters.length > 0 &&
+                        <div className='encounters_display_container'>
+                            <h3>Add options to your encounter! In each encounter players will have to choose from one of the three options you provide.</h3>
+                            {encounters}
+                            {encounters.length > 0 && <button className='btn add-another-encounter-btn' onClick={this.openNewEncounter}>Add Another Encounter</button>}
+                            {this.isValid() && <button className='btn' onClick={_=>this.completeStory()}>Complete Story</button>}
+                        </div>
+                    }
                     <div className='creator_display_container'>
                         {view}
                     </div>
                 </div>
+            </div>
             </div>
 
         );
